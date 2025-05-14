a34187fdf262cf43ce8fe2461e8eca0cb0bb27eb
Samuel Musyani <smusyani67@gmail.com>
2025-05-14 22:05:17 +0300

    DHT and LCD check!

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,16 @@ DHT dht(DHT_PIN, DHT_TYPE);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // Function declarations
-void dhtSensorCheck(float temp, float humid);
+bool dhtSensorCheck(float temp, float humid);
+void dhtMonitoring(float temp, float humid);
 
 void setup() {
   // Components' initialization
   dht.begin();  
   lcd.init();
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
   Serial.begin(9600);
   
   lcd.setCursor(0, 0);
@@ -32,14 +36,53 @@ void loop() {
   // Read temperature and humidity
   float temp = dht.readTemperature();
   float humid = dht.readHumidity();
-  
-  dhtSensorCheck(temp, humid); // Checks dht sensor's integrity
+
+  if (dhtSensorCheck(temp, humid)) {
+    dhtMonitoring(temp, humid);
+    delay(1500);
+    lcd.clear();
+  }
+
+  delay(750);
 }
 
-void dhtSensorCheck(float temp, float humid) {
+// Functions
+bool dhtSensorCheck(float temp, float humid) { // Checks dht sensor's integrity
   if (isnan(temp) || isnan(humid)) {
     Serial.println("DHT sensor at fault!");
     lcd.setCursor(0, 0);
     lcd.print("No DHT readings!");
+    digitalWrite(4, HIGH);
+    return false;
+  }
+  else {
+    digitalWrite(4, LOW);
+    return true;
+  }
+}
+
+void dhtMonitoring(float temp, float humid) { // Monitors and displays temperature and humidity
+  lcd.setCursor(0, 0);
+  String temperature = "Temp: " + String(temp) + "°C";
+  lcd.print(temperature);
+
+  if ((temp > 30) || (temp < 10)) {
+    digitalWrite(5, HIGH);
+    Serial.println("Temperature out of range, " + temperature);
+  }
+  else {
+    digitalWrite(6, LOW);
+  }
+
+  lcd.setCursor(0, 1);
+  String humidity = "Humid: " + String(humid) + "%";
+  lcd.print(humidity);
+
+  if ((humid > 60) || (humid < 40)){
+    digitalWrite(6, HIGH);
+    Serial.println("Humidity out of range, " + humidity);
+  }
+  else {
+    digitalWrite(6, LOW);
   }
 }
