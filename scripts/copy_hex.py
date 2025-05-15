cbd683ab24115b472f82033cd2c7ba31a610adb8
Samuel Musyani <smusyani67@gmail.com>
2025-05-15 11:48:36 +0300

    DHT, LCD & PIR check

---
+++ scripts/copy_hex.py
@@ -0,0 +1,12 @@
+import shutil
+from os.path import join
+
+Import("env")
+
+def copy_hex_file(source, target, env):
+    hex_path = join(env.subst("$BUILD_DIR"), env.subst("$PROGNAME") + ".hex")
+    dest_path = join(env.subst("$PROJECT_DIR"), "sim_circuit", env.subst("$PROGNAME") + ".hex")
+    shutil.copyfile(hex_path, dest_path)
+    print("Copied HEX to: " + dest_path)
+
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.hex", copy_hex_file)
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
+/* The system code aims to monitor the temperature, humidity and motion status on the storage facility. 
+It then should display the values from the sensors in the dedicated display (LCD for now).
+In case of any un-desired conditional change, the alert lights should come on and SMS alert should be sent to the system user.
+*/
+
 #include <Arduino.h>
 #include <DHT.h>
 #include <LiquidCrystal_I2C.h>
 
 // Definitions
-#define DHT_PIN 2     
+#define DHT_PIN 2    
 #define DHT_TYPE DHT11
+#define PIR_PIN 3
 
 // Object declarations
 DHT dht(DHT_PIN, DHT_TYPE);
@@ -13,14 +19,18 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 // Function declarations
 bool dhtSensorCheck(float temp, float humid);
 void dhtMonitoring(float temp, float humid);
+bool pirSensorCheck(int motion);
+void pirMonitoring(int motion);
 
 void setup() {
   // Components' initialization
   dht.begin();  
   lcd.init();
+  pinMode(PIR_PIN, INPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
   Serial.begin(9600);
   
   lcd.setCursor(0, 0);
@@ -37,8 +47,17 @@ void loop() {
   float temp = dht.readTemperature();
   float humid = dht.readHumidity();
 
+  // Read PIR value
+  int motion = digitalRead(PIR_PIN);
+
   if (dhtSensorCheck(temp, humid)) {
     dhtMonitoring(temp, humid);
+    delay(1500);
+    lcd.clear();
+  }
+
+  if (pirSensorCheck(motion)) {
+    pirMonitoring(motion);
     delay(1500);
     lcd.clear();
   }
@@ -47,7 +66,7 @@ void loop() {
 }
 
 // Functions
-bool dhtSensorCheck(float temp, float humid) { // Checks dht sensor's integrity
+bool dhtSensorCheck(float temp, float humid) { // Checks DHT sensor's integrity
   if (isnan(temp) || isnan(humid)) {
     Serial.println("DHT sensor at fault!");
     lcd.setCursor(0, 0);
@@ -71,7 +90,7 @@ void dhtMonitoring(float temp, float humid) { // Monitors and displays temperatu
     Serial.println("Temperature out of range, " + temperature);
   }
   else {
-    digitalWrite(6, LOW);
+    digitalWrite(5, LOW);
   }
 
   lcd.setCursor(0, 1);
@@ -84,5 +103,32 @@ void dhtMonitoring(float temp, float humid) { // Monitors and displays temperatu
   }
   else {
     digitalWrite(6, LOW);
+  }
+}
+
+bool pirSensorCheck(int motion) { // Checks integrity of PIR sensor
+  if (isnan(motion)) {
+    Serial.println("PIR sensor at fault!");
+    lcd.setCursor(0, 0);
+    lcd.print("No PIR detection");
+    digitalWrite(4, HIGH);
+    return false;
+  }
+  else {
+    digitalWrite(4, LOW);
+    return true;
+  }
+}
+
+void pirMonitoring(int motion) { // Checks and displays motion status
+  lcd.setCursor(0, 0);
+  if (motion) {
+    Serial.println("Motion detected!");
+    lcd.print("Rodent present!");
+    digitalWrite(7, HIGH);
+  }
+  else {
+    lcd.print("No motion!");
+    digitalWrite(7, LOW);
   }
 }
