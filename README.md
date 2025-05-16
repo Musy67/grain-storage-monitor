856c3f70f8c238bb02aa45a73bada84d1e176a31
Samuel Musyani <smusyani67@gmail.com>
2025-05-16 23:22:31 +0300

    Added sensor files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 The system code aims to monitor the temperature, humidity and motion status on the storage facility. 
 It then should display the values from the sensors in the dedicated display (LCD for now).
 In case of any un-desired conditional change, the alert lights should come on and SMS alert should be sent to the system user.
+
+
+
+As of 16th May, 2025, I want to continue integrate MQ-135 gas sensor for gas spoilage detection.
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
 #include <Arduino.h>
 #include <DHT.h>
 #include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
 
 // Definitions
 #define DHT_PIN 2    
 #define DHT_TYPE DHT11
 #define PIR_PIN 3
+#define sim900_RX 13
+#define sim900_TX 12  
 
 // Object declarations
 DHT dht(DHT_PIN, DHT_TYPE);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+SoftwareSerial simModule(sim900_RX, sim900_TX); // (TX, RX)
+
+// Global variables
+const String phoneNum = "+255773422381";
 
 // Function declarations
 bool sensorCheck(float temp, float humid, int motion);
 void dhtMonitoring(float temp, float humid);
 void pirMonitoring(int motion);
+void sendSMS(String phoneNum, String message);
 
 void setup() {
   // Components' initialization
@@ -26,12 +34,17 @@ void setup() {
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   Serial.begin(9600);
+  simModule.begin(9600);
   
+  // Displays welcome message
   lcd.setCursor(0, 0);
   lcd.print("Grain Storage");
   lcd.setCursor(5, 1);
   lcd.print("Monitor!");
   delay(1000);
+
+  // Test for SIM900 module
+  sendSMS(phoneNum, "Test for SMS!");
 }
 
 void loop() {
@@ -117,4 +130,18 @@ void pirMonitoring(int motion) { // Checks and displays motion status
   }
 }
 
-/* Put only one function to check the sensor''s integrity.*/
+void sendSMS(String phoneNum, String message) {
+  simModule.println("AT");  // Check comms
+  delay(500);
+
+  simModule.println("AT+CMGF=1"); // Set text mode
+  delay(500);
+
+  simModule.println("AT+CMGS=1\"" + phoneNum + "\""); // Set phone number
+  delay(500);
+
+  simModule.print(message);
+  delay(500);
+  simModule.write(26);  // Ctrl+Z to send
+  delay(5000);  
+}
